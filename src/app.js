cd9732e0059b067bb571fc6359567991ab9f0f59
himal999 <himal.official99@gmail.com>
2022-05-17 19:34:44 +0530

    server config

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hello MERN")
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+
+const app = express();
+
+app.use(cors());
+
+app.get('/',(req,res,next)=>{
+    res.send("<h1>Hello MERN Stack</h1>");
+    next();
+})
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT,()=>{
+    console.log(`server up on running ${PORT}`);
+})
